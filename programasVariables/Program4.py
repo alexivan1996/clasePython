daa3a3142b2225754bdeac59649837b2ceb99be8
alexivan1996 <alexivanflorespc.com>
2023-10-29 19:57:53 -0500

    primera clase

---
+++ programasVariables/Program4.py
@@ -0,0 +1,5 @@
+años =int(input("cuantos años ingreso a la empresa"))
+total = 2023-años
+bono = (total+120)-20
+print("monto a pagar",total)
+print("monto a pagar",bono)
---
+++ programasVariables/Programa3.py
@@ -0,0 +1,5 @@
+sb = float(input("ingresa el sueldo basico: "))
+bscuento = sb*0.10
+boni = sb*0.20
+sn = (sb-bscuento)+boni
+print("sueldo neto es",sn)
